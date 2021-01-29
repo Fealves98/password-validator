938496c57353d1f3fdd58066c33063d218da21c6
Felipe Alves <felipe.alves@mobbuy.com.br>
2021-01-29 11:36:20 -0300

    Criação da logica e testes unitários

--- src/main/java/com/itau/passwordValidator/web/request/PasswordRequest.java
+++ src/main/java/com/itau/passwordValidator/web/request/PasswordRequest.java
@@ -1,9 +1,11 @@
 package com.itau.passwordValidator.web.request;
 
 import lombok.Data;
+import org.apache.catalina.Session;
 
 @Data
 public class PasswordRequest {
 
     private String password;
+
 }
